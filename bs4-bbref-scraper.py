67786cc5d1e6a59fe786c2a72f9115df57c7fbfc
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-03-31 13:47:53 -0400

    Update bs4-bbref-scraper.py

--- bs4-bbref-scraper.py
+++ bs4-bbref-scraper.py
@@ -3,7 +3,7 @@
 class bbref_register():
 
     def __init__(self):
-        print(" --- Scrape the Baseball Reference Register ---")
+        print(" --- baseball-ref-register scraper initialized ---")
 
     # This only gets me the first table from each page (only team_batting from team_page)
     def find_first_table_data(self, url):
@@ -42,54 +42,29 @@ class bbref_register():
         player_data = []
         for link in tqdm(player_links):
             player_data.append(self.find_first_table_data('https://www.baseball-reference.com' + link))
-        # add unique id numbers for each player since we didnt scrape names
-        id_num = 0
-        for player in player_data:
-            player["ID"] = id_num 
-            id_num += 1       
         return player_data
 
-    def get_league_player_background_history(self, dictionary_of_year_ids):
+    def get_league_player_background_history(self, list_of_year_ids):
         print("\n --- Parsing Player Background Data by Year ---")
-        self.lg_background_players = [self.get_player_background_data(year) for year in dictionary_of_year_ids.values()]
+        self.lg_background_players = [self.get_player_background_data(year) for year in list_of_year_ids]
         return self.lg_background_players
 
-
-    """ Replacing the get_league_player_background_history function with an easier one to work with """
-    """ Not sure if it works yet"""
-    def get_all_league_history(self, league_homepage_link_identifier):
-        print(" --- Getting all league and player history ---")
-        def_url = "https://baseball-reference.com"
-        links_to_search_through = self.find_links(def_url + "/register/league.cgi")
-        the_league = []
-        for link in links_to_search_through:
-            if league_homepage_link_identifier in link:
-                the_league.append(def_url + link)
-        links_from_league_page = [self.find_links(link) for link in the_league] 
-        team_links = []
-        for link in links_from_league_page:
-            if "/register/team.cgi?id=" in link:
-                team_links.append(def_url + link)
-        return [self.find_first_table_data(team) for team in team_links]
-
-
     def flip_my_data(self, list_of_parsed_league_data):
-        mooshed_data_by_year = [pd.concat(year) for year in list_of_parsed_league_data]
-        flat_list = []
-        for year in mooshed_data_by_year:
-            flat_list.append(year)
-        self.all_data = pd.concat(flat_list)
-        # Make numeric
-        bat = self.all_data.apply(pd.to_numeric, errors='coerce').combine_first(self.all_data)
-        pit = self.all_data.apply(pd.to_numeric, errors='coerce').combine_first(self.all_data)
-        return self.flip(self.all_data)
+            mooshed_data_by_year = [pd.concat(year) for year in list_of_parsed_league_data]
+            flat_list = []
+            for year in mooshed_data_by_year:
+                flat_list.append(year)
+            self.all_data = pd.concat(flat_list)
+            # Make numeric
+            self.all_data = self.all_data.apply(pd.to_numeric, errors='coerce').combine_first(self.all_data)
+            return self.flip(self.all_data)
 
-    # sqlite function(s)
-    def move_to_sql(self, df, data_base_name, table_name):
-        print(f" --- Moved Data To Local sqlite3 Database ({data_base_name}, {table_name}) ---")
-        self.conn = sqlite3.connect(data_base_name + ".db")
-        self.cur = self.conn.cursor()
-        return df.to_sql(name = table_name, con = self.conn, if_exists = "replace")
-    
-    def end_sql(self):
-        return self.conn.close()
+        # sqlite function(s)
+        def move_to_sql(self, df, data_base_name, table_name):
+            print(f" --- Moved Data To Local sqlite3 Database ({data_base_name}, {table_name}) ---")
+            self.conn = sqlite3.connect(data_base_name + ".db")
+            self.cur = self.conn.cursor()
+            return df.to_sql(name = table_name, con = self.conn, if_exists = "replace")
+
+        def end_sql(self):
+            return self.conn.close()
