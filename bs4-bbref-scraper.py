888061d844a1ea677b2225b15472c3a9cd96f299
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-05-03 00:22:19 -0400

    Update bs4-bbref-scraper.py

--- bs4-bbref-scraper.py
+++ bs4-bbref-scraper.py
@@ -10,46 +10,49 @@ class bbref_register():
 
     def __init__(self):
         print(" --- baseball-ref-register scraper initialized ---")
-
-    # This only gets me the first table from each page (only team_batting from team_page)
-    def find_first_table_data(self, url):
-        return pd.read_html(url)
-
+        
     def sewp(self, url):
-        webpage = requests.get(url)
-        return BeautifulSoup(webpage.text, features = 'lxml')
+        r = requests.get(url)
+        return BeautifulSoup(r.text, features = 'lxml')
 
     def find_links(self, url):
         html = self.sewp(url)
         href_tags = html.find_all(href = True)
-        return [tag.get('href') for tag in href_tags]
+        return [tag['href'] for tag in href_tags]
 
-    # Baseball reference specific functions
-    def get_player_background_data(self, league_year_id):
-        def get_player_background_data(self, league_year_id):
-        def_url = "https://baseball-refernece.com"
-        league_links = self.find_links(def_url + "/register/league.cgi?id=" + league_year_id)
-        team_links = [link for link in league_links if "/register/team.cgi?" in link]
-        links_from_team = [self.find_links(def_url + link) for link in team_links]
-        player_links = list(set([link for link in links_from_team if "/register/player.fcgi?" in link]))
-        player_data = [self.find_first_table_data(def_url + link) for link in tqdm(player_links)]
-        # add unique id numbers for each player since we didnt scrape names
-        id_num = 0
-        for player in player_data:
-            player["ID"] = id_num 
-            id_num += 1
-        return player_data
-        # Add id_number for each player since we didn't scrape names
-        id_num = 0
-        for player in player_data:
-            player["ID"] = id_num 
-            id_num += 1       
-        return player_data
-    
-    def get_league_player_background_history(self, list_of_year_ids):
-        print("\n --- Parsing Player Background Data by Year ---")
-        self.lg_background_players = [self.get_player_background_data(year) for year in list_of_year_ids]
-        return self.lg_background_players
+    def tbl_data(self, url):
+        return pd.read_html(url)[0]
+
+    def player_info(self, url):
+        soup = self.sewp(url)
+        # Get text from tags where player info is
+        spans = soup.find_all(['span','strong'])
+        ps = soup.find_all('p')
+        ps = [tag.text for tag in ps]
+        spans = [tag.text for tag in spans]
+        data = self.tbl_data(url)
+        data['name'] = spans[7]
+        data['height'] = ps[2][0:4].replace('-','.')
+        data['weight(kg)'] = ps[2][-6:].replace(' ','').replace(')','').replace('kg','')
+        data['bats'] = ps[1][6:12].replace('\\', '').replace(' ','')
+        data['throws'] = ps[1][-12:].replace(':','').replace('\n','').replace(' ','')
+        data['position'] = ps[0][-18:].replace(' ','').replace('\n', '').replace('n:','')
+        return data
+
+    def get_links(self, url, identifier):
+        soup = self.sewp(url)
+        new_def_url = 'https://baseball-reference.com'
+        hrefs_tags = soup.find_all('a', href = True)
+        return [(new_def_url + tag['href']) for tag in hrefs_tags if identifier in tag['href']]
+
+    def get_plyr_hist(self, br_lg_id):
+        new_def_url = 'https://baseball-reference.com'
+        tm_links = self.get_links(new_def_url + br_lg_id, 'team.cgi?id=')
+        print('Collecting Player links from team pages...')
+        plyr_links = [self.get_links(team, '/player.fcgi?id=') for team in tqdm(tm_links)]
+        print('Collecting player Data...')
+        player_list = list(set([player for team in plyr_links for player in team]))
+        return pd.concat([self.player_info(player) for player in tqdm(player_list)])
 
     def flip_my_data(self, list_of_parsed_league_data):
             mooshed_data_by_year = [pd.concat(year) for year in list_of_parsed_league_data]
